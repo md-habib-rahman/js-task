a259915ba757caf64e9844864e9f640408c1e5a5
Md. Habibur Rahman Bappy <habib.rahman@live.com>
2025-02-11 17:52:53 +0600

    task 2

---
+++ task-2.js
@@ -0,0 +1,11 @@
+let touristDestination = ["Sundorbon", "cox's bazaar", "Sylhet"];
+
+touristDestination.push("Khulna");
+
+touristDestination.push("Lalbag Kella");
+
+touristDestination.push("National Museum");
+
+touristDestination.pop();
+
+console.log(touristDestination);
