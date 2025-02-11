f635ca70df2767eb37f10cb9ac0624ed143a201f
Md. Habibur Rahman Bappy <habib.rahman@live.com>
2025-02-11 18:02:03 +0600

    task 5

---
+++ task-5.js
@@ -0,0 +1,9 @@
+let touristDestination = ["Sundorbon", "cox's bazaar", "Sylhet"];
+
+let fruites = ["mango", "banana", "lichi", "papaya", "plam"];
+
+let newArray = touristDestination.concat(fruites);
+
+console.log(touristDestination);
+console.log(fruites);
+console.log(newArray);
